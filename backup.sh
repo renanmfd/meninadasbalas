0b378f47c3fd08c94df21af93a375066ffcb8f39
Renan Dias <renanmfd@gmail.com>
2017-05-20 12:13:07 -0300

    Init backup

---
+++ backup.sh
@@ -0,0 +1,22 @@
+BASEPATH=${PWD};
+MYSQL_USER="root"
+MYSQL_PSWD="res2res2"
+MYSQL_TABLE="meninadasbalas"
+SITENAME=$MYSQL_TABLE;
+DATE=$(date "+%Y-%m-%d-%H-%M-%S")
+
+mkdir -p backup/file
+mkdir -p backup/database
+
+echo $BASEPATH
+DUMPNAME="$SITENAME-$DATE.sql"
+cd "$BASEPATH/backup/database/"
+mysqldump -u$MYSQL_USER -p$MYSQL_PSWD $MYSQL_TABLE > $DUMPNAME
+zip -r -q "$DUMPNAME.zip" $DUMPNAME
+rm $DUMPNAME
+
+cd "$BASEPATH/web/sites/default/";
+FILENAME="$BASEPATH/backup/file/$SITENAME-$DATE.zip"
+zip -r -q $FILENAME files/
+cd $BASEPATH
+
