63af6a41ab9e947c307fd84ec251700a7412d281
Renan Dias <renanmfd@gmail.com>
2017-05-28 22:27:20 -0300

    Gulpfy javascrpt files

---
+++ themes/custom/mbc_theme/js/src/home.js
@@ -0,0 +1,15 @@
+(function ($, Drupal) {
+  "use strict";
+  /*global jQuery:false */
+  /*global Drupal:false */
+  /*global console:false */
+
+  /**
+   * Activate carousel homepage.
+   */
+  Drupal.behaviors.someBehavior = {
+    attach: function (context) {
+    }
+  };
+
+}(jQuery, Drupal));
